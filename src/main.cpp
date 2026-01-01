6b13f02b606c2530bda2a7a96d5a52d205722de6
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-01-01 21:30:23 +0100

    +

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
+#include <thread>
+#include <vector>
 #include <orbis/SystemService.h>
 #include <orbis/libkernel.h>
+#include <unistd.h>
 #include "assert.h"
 #include "logging.h"
 #include "types.h"
 
-int main(void) {
-    LOG_INFO("Hello, {}!", "Klog");
-    LOG_NOTIFICATION("Hello, {}!", "Screen");
+struct DT {
+    DT() {
+        thread_local int at_dtor_time = 0;
+        LOG_INFO("hey world (at_dtor_time: {})", at_dtor_time);
+        ++at_dtor_time;
+    }
+    inline static thread_local int istli;
+};
+DT& evil_singleton() {
+    thread_local DT wow;
+    return wow;
+}
 
+int main() {
+    LOG_INFO("Starting homebrew");
+    auto t1 = std::thread([](){
+        DT& wawa1 = evil_singleton();
+        DT::istli = 2;
+        LOG_INFO("addr: {} istli: {} {}", fmt::ptr(&wawa1), fmt::ptr(&DT::istli), DT::istli);
+    });
+    DT& wawa = evil_singleton();
+    DT::istli = 1;
+    LOG_INFO("addrs: {} {}", fmt::ptr(&wawa), fmt::ptr(&DT::istli));
+    ++DT::istli;
+    LOG_INFO("istli: {}", DT::istli);
     sceSystemServiceLoadExec("EXIT", nullptr);
     return 0;
 }
